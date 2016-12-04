03a0fd8dda0c811c19d00d0dcf1371a5ff674550
Ishmeet Singh <ishmeet.1136@gmail.com>
2016-12-04 16:37:33 +0530

    FusedLocationAPI init

---
+++ FusedLocationAPI/app/src/main/java/com/example/warmachine/locationproject/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.warmachine.locationproject;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final String LOG_TAG = "Location";
+    private TextView longitute;
+    private TextView latitude;
+    private GoogleApiClient googleApiClient;
+    private Location locationRequest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        longitute = (TextView) findViewById(R.id.txt);
+        latitude = (TextView) findViewById(R.id.txt1);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationRequest =  LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if(locationRequest != null) {
+                latitude.setText(String.valueOf(locationRequest.getLatitude()));
+                longitute.setText(String.valueOf(locationRequest.getLongitude()));
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
